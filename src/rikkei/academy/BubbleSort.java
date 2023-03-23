4c5931dcc5c868b83dbb63a7c4196742fc6cd0ac
duongtt1 <trinhtungduong2019@gmail.com>
2023-03-23 16:41:41 +0700

    MD3_Lesson14_Practice1_Update

--- src/rikkei/academy/BubbleSort.java
+++ src/rikkei/academy/BubbleSort.java
@@ -6,10 +6,10 @@ public class BubbleSort {
     public static void bubbleSort(int[] list) {
         boolean needNextPass = true;
 
-        for (int k = 0; k < list.length && needNextPass; k++) {
+        for (int k = 0; k < list.length - 1 && needNextPass; k++) {
             // Array may be sorted and next pass not needed
             needNextPass = false;
-            for (int i = 0; i < list.length - k; i++) {
+            for (int i = 0; i < list.length - k - 1; i++) {
                 if (list[i] > list[i + 1]){
                     // Swap list[i] with list[i + 1]
                     int temp = list[i];
@@ -25,7 +25,7 @@ public class BubbleSort {
     public static void main(String[] args) {
         bubbleSort(list);
         for (int i = 0; i < list.length; i++) {
-            System.out.println(list[i] + " ");
+            System.out.print(list[i] + " ");
         }
     }
 }
